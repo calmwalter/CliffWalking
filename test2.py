0017d6f03aee2616cc51d6f529af5f9b2a0a6c27
calmwalter <calmwalter@outlook.com>
2019-07-04 10:52:00 +0800

    a simple change

--- test2.py
+++ test2.py
@@ -1,14 +1,14 @@
 from VisualCliff import *
 
 # 初始化网格用来记录权值
-row = 4
-column = 8
+row = 8
+column = 16
 
-net = np.zeros(row*column*4).reshape(4, 8, 4)
+net = np.zeros(row*column*4).reshape(row, column, 4)
 
 
 # 初始化学习率和折扣因素
 alpha = 0.1
 discount_factor = 0.2
-run(net, 0, 0, alpha, discount_factor, 0, column-1, row, column,500)
+run(net, 0, 0, alpha, discount_factor, 0, column-1, row, column,1000)
 print(net)
